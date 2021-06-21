65e5588818cb1bd6c33b8009144a2690b71cf17f
Iтѕ_Ѵιcнч#1337 <66045929+Its-Vichy@users.noreply.github.com>
2021-06-21 06:04:48 +0000

    ✔️ 0.0.2
    ⚙️ Handle:
        - Url / Proxy not found
        - CloudFare ban
        - Error when create thread
    
    🔨 Patch
        - Console error when price is so long
        - Embed color
    
    ⭐ New:
        - Spoof request's referer
        - Add banned proxy

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ from colorama import Fore, Style, init; init()
 from fake_useragent import UserAgent
 from requests import Session, post
 from threading import Thread
-import time, math
+import time, random, sys
 
 class CTB():
     def __init__(self, webhook):
-        self.error_proxy = []
-        self.used_proxy  = []
-        self.proxy_list  = []
+        self.banned_proxy = []
+        self.error_proxy  = []
+        self.used_proxy   = []
+        self.proxy_list   = []
         self.hook   = webhook
+        self.banned = 0
         self.botted = 0
         self.error  = 0
         self.url = []
     
     '''
     Charger les fichiers dans les listes, et retirer les proxy/url dupliqués
+        New:
+            * Handle proxy / url not found
     '''
     def load_files(self):
         with open('./sources/url.txt', 'r') as url_file:
@@ -29,8 +33,19 @@ class CTB():
         self.proxy_list = list(set(self.proxy_list))
         self.url        = list(set(self.url))
 
+        if len(self.proxy_list) <= 5:
+            print(f'[{Fore.LIGHTRED_EX}{time.strftime("%H:%M:%S", time.localtime())}{Fore.RED}] ERROR: not enought proxy found, 5 proxies required', end='\n')
+            sys.exit(0)
+
+        if len(self.url) == 0:
+            print(f'[{Fore.LIGHTRED_EX}{time.strftime("%H:%M:%S", time.localtime())}{Fore.RED}] ERROR: not enought url found, 1 proxies required', end='\n')
+            sys.exit(0)
+
     '''
     Envoyer une noctification sur discord avec les statistiques du bot
+        New:
+            * Banned proxy
+            * New embed color
     '''
     def send_hook(self):
         try:
@@ -38,7 +53,7 @@ class CTB():
                 'content': None,
                 'embeds': [
                             {
-                                'color': None,
+                                'color': 0x4077f8,
                                 'fields': [
                                     {
                                     'name': '> ⭐ **Estimated earn:**',
@@ -51,10 +66,14 @@ class CTB():
                                     {
                                     'name': '> 🔴 **Error:**',
                                     'value': f'```{self.error}```'
+                                    },
+                                    {
+                                    'name': '> 🔴 **Banned:**',
+                                    'value': f'```{self.banned}```'
                                     }
                                 ],
                                 'footer': {
-                                    'text': 'CTB By Ѵιcнч\"RCΛ#1337 - github.com/Its-Vichy',
+                                    'text': 'CTB By Ѵιcнч\"RCΛ#1337 - github.com/Its-Vichy - discord.gg/rca',
                                     'icon_url': 'https://photos.angel.co/startups/i/7641023-67bf91ba0e7f458607f981d1813d0ef8-medium_jpg.jpg?buster=1614716185'
                                 }
                             }
@@ -67,36 +86,42 @@ class CTB():
 
     '''
     Mettre à jour la console toute les secondes
-        Todo:
-            * Couleurs
-            * Pourcentages
+        New:
+            * Banned proxy
+            * Error when price is so long
     '''
     def update_gui(self):
         while not len(self.used_proxy) == len(self.proxy_list):
-            time.sleep(1)   
+            time.sleep(1)
             self.used_proxy  = list(set(self.used_proxy))
             self.error_proxy = list(set(self.error_proxy))
-            print(f'[{Fore.LIGHTRED_EX}{time.strftime("%H:%M:%S", time.localtime())}{Fore.WHITE}] BOT: {Fore.GREEN}{self.botted}{Fore.WHITE} ERROR: {Fore.YELLOW}{self.error}{Fore.WHITE} USED PROXY: {Fore.LIGHTMAGENTA_EX}{len(self.used_proxy)}{Fore.WHITE}/{Fore.MAGENTA}{len(self.proxy_list)}{Fore.WHITE} EARN ~{Fore.CYAN}{self.botted * 0.0025}{Fore.WHITE}€', end='\r') #  [{Fore.LIGHTRED_EX}{"#" * (math.ceil(len(self.used_proxy)/10))}{"-" * ((math.floor(len(self.proxy_list)) / 10) - math.ceil(len(self.used_proxy))) / 10}{Fore.WHITE}] {math.floor(len(self.proxy_list))}% 
+            print(f'[{Fore.LIGHTRED_EX}{time.strftime("%H:%M:%S", time.localtime())}{Fore.WHITE}] BOT: {Fore.GREEN}{self.botted}{Fore.WHITE} ERROR: {Fore.YELLOW}{self.error}{Fore.WHITE} BANNED: {Fore.LIGHTYELLOW_EX}{self.banned}{Fore.WHITE} USED PROXY: {Fore.LIGHTMAGENTA_EX}{len(self.used_proxy)}{Fore.WHITE}/{Fore.MAGENTA}{len(self.proxy_list)}{Fore.WHITE} EARN ~{Fore.CYAN}{self.botted * 0.0025}{Fore.WHITE}€             ', end='\r')
          
         BOT.send_hook()
 
     '''
     Un worker
+        New:
+            * Handle cloudfare ban
+            * Spoof referer
     '''
     def worker(self, url, proxy_list):
         for proxy in proxy_list:
-            time.sleep(0.5)
-            if proxy not in self.error_proxy:
+            if proxy not in self.error_proxy or proxy not in self.banned_proxy:
                 self.used_proxy.append(proxy)
 
                 try:
                     session = Session()
-                    session.headers = {'user-agent': UserAgent().random}
+                    session.headers = {'user-agent': UserAgent().random, 'referer': 'https://' + random.choice(['github.com', 'discord.com', 'twitter.com', 'facebook.com', 'youtube.com'])}
                     session.proxies = {'http': proxy, 'https': proxy}
                     response = session.get('https://www.mylink1.biz/link/redirect/?url=' + session.get(url).text.split('https://www.mylink1.biz/link/redirect/?url=')[1].split('">')[0])
 
                     if response.status_code == 200:
-                        self.botted += 1
+                        if 'Cloudflare Ray' not in response.text:
+                            self.botted += 1
+                        else:
+                            self.banned_proxy.append(proxy)
+                            self.banned += 1
                     
                 except:
                     self.error_proxy.append(proxy)
@@ -121,6 +146,8 @@ class CTB():
 
     '''
     Démarer tous les threads
+        New:
+            * Handle thread creation limit
     '''
     def start_worker(self):
         thread_list = []
@@ -128,8 +155,11 @@ class CTB():
         thread_list.append(Thread(target= self.update_gui))
         for url in self.url:
             for proxys in list(zip(*[iter(self.proxy_list)] * self.get_perfect_number(len(self.proxy_list))[1])):
-                thread_list.append(Thread(target= self.worker, args=(url, list(proxys),)))
-        
+                try:
+                    thread_list.append(Thread(target= self.worker, args=(url, list(proxys),)))
+                except:
+                    break
+
         for thread in thread_list:
             thread.start()
 
@@ -141,7 +171,7 @@ if __name__ == '__main__':
    ___ _____ ___ 
   / __|_   _| _ )
  | (__  | | | _ \\
-  \___| |_| |___/  Ѵιcнч"RCΛ#1337 | https://github.com/Its-Vichy
+  \___| |_| |___/  Ѵιcнч"RCΛ#1337 | https://github.com/Its-Vichy | discord.gg/rca
                  
 
     ''' + Style.RESET_ALL)
